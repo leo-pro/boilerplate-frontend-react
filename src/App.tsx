34603be5ab8f2c18118169ae114a0d679c1edf77
Leonardo Alves <leoroberto.alves@hotmail.com>
2022-01-14 21:36:49 -0300

    create project structure with eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import logo from './logo.svg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Complete Boilerplate for ReactJS Applications</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/leo-pro/boilerplate-frontend-react"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go to Repo
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
